ed0f211aac348431c4fb91c14745fb8314ae1da7
olzhasar <o.arystanov@gmail.com>
2023-05-18 18:48:40 +0600

    Solve 01/p2

---
+++ 01/p2.c
@@ -0,0 +1,48 @@
+#import <stdio.h>
+#import <stdlib.h>
+#import <string.h>
+
+int first = 0, second = 0, third = 0;
+
+void updateMax(int val) {
+  if (val > first) {
+    third = second;
+    second = first;
+    first = val;
+  } else if (val > second) {
+    third = second;
+    second = val;
+  } else if (val > third) {
+    third = val;
+  }
+}
+
+
+int main() {
+  FILE *fp;
+  int bufferlength = 255;
+  char line[bufferlength];
+
+  fp = fopen("input.txt", "r");
+
+  int rolling_max = 0;
+
+  while(fgets(line, bufferlength, fp) != NULL) {
+    if (strlen(line) <= 1) {
+      updateMax(rolling_max);
+      rolling_max = 0;
+      continue;
+    }
+
+    rolling_max += atoi(line);
+  }
+
+  updateMax(rolling_max);
+
+  fclose(fp);
+
+  printf("Top three:\n%d\n%d\n%d\n\n", first, second, third);
+  printf("Total:\n%d\n", first + second + third);
+
+  return 1;
+}
